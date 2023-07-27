8395a8dc9087b376978e48e1612440f7d1f7f06c
DarkKnight714 <140713891+DarkKnight714@users.noreply.github.com>
2023-07-27 21:19:27 +0530

    Add files via upload

---
+++ readdataforipy.py
@@ -0,0 +1,6 @@
+filename = "5l.txt"
+with open(filename) as fopen:
+    doc = [line.split() for line in fopen]
+
+doc.sort(reverse=True)
+print(doc)
---
+++ readdataforpypy.py
@@ -0,0 +1,10 @@
+filename="5l.txt"
+fopen=open(filename)
+doc=[]
+for line in fopen:
+    x=line.split()
+    doc.append(x)
+doc.sort(reverse=True)
+print(doc)
+
+
